702f67d20ce338533832b270c5155754ad26c1a4
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-01-27 22:57:54 +0900

    230127 baekjoonlv
    동적계획법1

--- baekjoonlv/src/동적계획법/Stairs.java
+++ baekjoonlv/src/동적계획법/Stairs.java
@@ -2,6 +2,7 @@ package 동적계획법;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 //계단오르기
 public class Stairs {
@@ -9,18 +10,29 @@ public class Stairs {
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[t];
- 		for(int i = 0; i < t; i++) {
+		int[] arr = new int[n+1];
+		int[] dp = new int[n+1];
+  		for(int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
- 		int sum = 0;
- 		for(int i = 1; i <= t; i++) {
- 			
+  		dp[1] = arr[1];
+  		
+  		if(n >= 2) {
+  			dp[2] = arr[1] + arr[2];
+  		}
+  		
+ 		for(int i = 3; i <= n; i++) {
+ 			//n-3, n-1, n 또는 n-2, n
+ 			dp[i] = Math.max(dp[i-3] + arr[i-1], dp[i-2]) + arr[i];
  		}
  		
+ 		System.out.println(Arrays.toString(dp));
+ 		
+ 		System.out.println(dp[n]);
+ 		br.close();
 	}
 
 }
