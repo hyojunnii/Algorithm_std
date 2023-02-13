53e6d9daf6f2404a86ce54b8620265c6aa6de1e5
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-02-13 19:28:25 +0900

    230213 backjoonlv
    큐

---
+++ baekjoonlv/src/큐/Queue2.java
@@ -0,0 +1,56 @@
+package 큐;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Queue2 {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+
+		//또는 Deque - peeklast
+		LinkedList<Integer> que = new LinkedList<Integer>();
+		
+		StringTokenizer st;
+		while(N-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			switch (st.nextToken()) {
+			case "push":
+				que.offer(Integer.parseInt(st.nextToken()));
+				break;
+			case "pop":
+				//큐는 비어있으면 널값 반환
+				//if(item == null)
+				if(que.isEmpty()) sb.append(-1).append('\n');
+				else sb.append(que.poll()).append('\n');
+				break;
+			case "size":
+				sb.append(que.size()).append('\n');
+				break;
+			case "empty":
+				if(que.isEmpty()) sb.append(1).append('\n');
+				else sb.append(0).append('\n');
+				break;
+			case "front":
+				if(que.isEmpty()) sb.append(-1).append('\n');
+				else sb.append(que.peek()).append('\n');
+				break;
+			case "back":
+				if(que.isEmpty()) sb.append(-1).append('\n');
+				else sb.append(que.peekLast()).append('\n');
+				break;
+			}
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+
+}
---
+++ baekjoonlv/src/큐/QueueMaterialize.java
@@ -0,0 +1,90 @@
+package 큐;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//큐 직접구현
+public class QueueMaterialize {
+	
+	static int[] q = new int[2000000];
+	
+	static int size = 0;
+	static int front = 0;
+	static int back = 0;
+
+    static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		while(N-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			switch(st.nextToken()) {
+			case "push": push(Integer.parseInt(st.nextToken())); break;
+			case "pop": pop(); break;
+			case "size": size(); break;
+			case "empty": empty(); break;
+			case "front": front(); break;
+			case "back": back(); break;
+			}
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+	
+	static void push(int n) {
+		q[back] = n;
+		back++;
+		size++;
+	}
+	
+	static void pop() {
+		if(size == 0) {
+			sb.append(-1).append('\n');
+		}
+		else {
+			sb.append(q[front]).append('\n');
+			size--;
+			front++;
+		}
+	}
+	
+	static void size() {
+		sb.append(size).append('\n');
+	}
+	
+	static void empty() {
+		if(size == 0) {
+			sb.append(1).append('\n');
+		}
+		else {
+			sb.append(0).append('\n');
+		}
+	}
+	
+	static void front() {
+		if(size == 0) {
+			sb.append(-1).append('\n');
+		}
+		else {
+			sb.append(q[front]).append('\n');
+		}
+	}
+	
+	static void back() {
+		if(size == 0) {
+			sb.append(-1).append('\n');
+		}
+		else {
+			sb.append(q[back-1]).append('\n');
+		}
+	}
+
+}
