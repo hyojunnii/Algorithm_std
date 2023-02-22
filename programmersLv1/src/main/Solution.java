3ca70b6787cd4551224d5985212df312d1e7c364
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-02-22 22:47:56 +0900

    230222 programmers

--- programmersLv1/src/main/Solution.java
+++ programmersLv1/src/main/Solution.java
@@ -2,14 +2,12 @@ package main;
 
 public class Solution {
 	
-	void solution() {
+	public static void solution() {
 		
 	}
 
 	public static void main(String[] args) {
-		
-		Solution s = new Solution();
-		s.solution();
+		solution();
 		
 	}
 
---
+++ programmersLv1/src/피보나치_수/Solution.java
@@ -0,0 +1,21 @@
+package 피보나치_수;
+
+public class Solution {
+	
+	public static int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+
+	public static void main(String[] args) {
+		solution(2);
+	}
+
+}
