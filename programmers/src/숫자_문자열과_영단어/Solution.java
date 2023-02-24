1442cad53d1cef5f63e4c226c29f7d947bdb6be6
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-02-24 23:18:23 +0900

    230224 programmers

---
+++ programmers/src/숫자_문자열과_영단어/Solution.java
@@ -0,0 +1,15 @@
+package 숫자_문자열과_영단어;
+
+public class Solution {
+	
+	public static void solution(String s) {
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		solution("one4seveneight");
+	}
+
+}
